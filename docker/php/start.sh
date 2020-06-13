f2efbc56c4e1fe842baa228d1d67123998e598a0
cyradin <cyradin@protonmail.com>
2020-06-13 17:56:52 +0200

    Enable RoadRunner dev server

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-(composer install && (test -f /srv/bin/rr || /srv/vendor/bin/rr get-binary --location /srv/bin) && /srv/bin/rr -v -d serve) || (echo $1 && tail -f /dev/null)
+
+{
+  composer install
+  test -f /srv/bin/rr || /srv/vendor/bin/rr get-binary --location /srv/bin
+  if [ ${DEV_MODE} == 1 ]
+  then
+    echo "Starting dev server"
+    /srv/bin/rr -v -d -c /srv/.rr.dev.yaml serve
+  else
+    echo "Starting prod server"
+    /srv/bin/rr -v -d -c /srv/.rr.yaml serve
+  fi
+} || {
+    echo $1 && tail -f /dev/null
+}
